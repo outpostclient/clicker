4ef99248de09b2e62b78a2e7498018e102c72b51
outpostclient <outpostclient@gmail.com>
2025-01-13 17:09:56 +0530

    add changes

--- clicker/settings.py
+++ clicker/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django_brotli.middleware.BrotliMiddleware',  # Add BrotliMiddleware
+    # 'django_brotli.middleware.BrotliMiddleware',  # Add BrotliMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
